22e9063f6bad06b8b414031bcf2ac8c440ebcfd5
etabegro <43544501+etabegro@users.noreply.github.com>
2022-01-28 16:15:04 +0100

    Shorten lines to conform to standard.

--- src/flake8_json_reporter/reporters.py
+++ src/flake8_json_reporter/reporters.py
@@ -159,5 +159,7 @@ class CodeClimateJSON(base.BaseFormatter):
         return string
 
     def show_source(self, violation):
-        """Codeclimate does not have a good place to show the source. Return None."""
+        """ Codeclimate does not have a good place to show the source. 
+        Return None instead.
+        """
         return None
